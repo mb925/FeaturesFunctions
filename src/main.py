be5466e306d649246ee770b46cb4266e9781fd35
martina <martina.bevilacqua1992@gmail.com>
2021-06-07 17:47:29 +0200

    add multiple terms

--- src/main.py
+++ src/main.py
@@ -8,7 +8,14 @@ import seaborn as sns # for data visualization
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    list_terms = ['00050', '00063',  '00071',  '00072',  '00074', '00056', '00064']
+    list_terms = ['00077', '00078',  '00051',  '00052',  '00053', '00055',
+                  '00058',  '00060', '00063', '00065',
+                  '00066', '00071', '00073', '00074', '00002', '00003',
+                  '00005', '00006', '00007', '00009', '00011', '00012',
+                  '00014', '00015', '00016', '00019', '00020', '00021',
+                  '00022', '00023', '00025', '00026', '00027', '00028',
+                  '00029', '00032', '00033', '00034', '00037', '00038',
+                  '00041', '00042', '00043', '00046', '00047']
     list_features = ['prediction-low_complexity-merge', 'prediction-proline_rich-mobidb_lite_sub',
                      'prediction-lip-anchor']
     parser.create_protein_terms_features_table(list_terms, list_features)
--- src/parser.py
+++ src/parser.py
@@ -9,6 +9,57 @@ import json
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+terms_names = {
+
+    '00078': 'pre-molten globule',
+    '00077': 'molten globule',
+    '00051': 'disorder to molten globule',
+    '00052': 'disorder to pre-molten globule',
+    '00053': 'molten globule to order',
+    '00055': 'pre-molten globule to order',
+    '00058': 'Order to pre-molten globule',
+    '00060': 'Pre-molten globule to molten globule',
+    '00063': 'Protein binding',
+    '00065': 'DNA binding',
+    '00066': 'RNA binding',
+    '00071': 'Lipid binding',
+    '00073': 'Metal ion binding',
+    '00074': 'Small molecule binding',
+    '00002': 'Flexible linker/spacer',
+    '00003': 'Entropic bristle',
+    '00005': 'Entropic spring',
+    '00006': 'Structural mortar',
+    '00007': 'Self-transport through channel',
+    '00009': 'Assembler',
+    '00011': 'Targeting',
+    '00012': 'Tethering',
+    '00014': 'Neutralization of toxic molecules',
+    '00015': 'Metal binding',
+    '00016': 'Water storage',
+    '00019': 'Cis-regulatory element',
+    '00020': 'Disassembler',
+    '00021': 'Activator',
+    '00022': 'DNA bending',
+    '00023': 'DNA unwinding',
+    '00025': 'Regulation of phosphorylation',
+    '00026': 'Regulation of acetylation',
+    '00027': 'Regulation of methylation',
+    '00028': 'Regulation of glycosylation',
+    '00029': 'Regulation of ubiquitination',
+    '00032': 'Regulation of palmitoylation',
+    '00033': 'Regulation of limited proteolysis',
+    '00034': 'Regulation of ADP-ribosylation',
+    '00037': 'Space filling',
+    '00038': 'Entropic exclusion',
+    '00041': 'Liquid-liquid phase separation',
+    '00042': 'Hydrogel',
+    '00043': 'Aggregate',
+    '00046': 'Amyloid',
+    '00047': 'Prion',
+
+}
+
+
 def create_protein_terms_features_table(terms, features):
 
     # for term in terms:
@@ -23,16 +74,17 @@ def create_protein_terms_features_table(terms, features):
 
 
 def parse_file_protein_terms(term):
-    print(term)
+    # print(term)
 
     with open(cfg.data['data'] + 'disprot_regions.json') as json_file:
         data = json.load(json_file)
         count = 0
+        flag_term = False
+
         for el in data['data']:
             count += 1
             sequence_length = len(el['sequence'])
             term_column = [0] * sequence_length
-            flag_term = False
 
 
             for region in el['regions']:
@@ -47,6 +99,9 @@ def parse_file_protein_terms(term):
                     os.makedirs(term_dir)
                 df = pd.DataFrame(term_column, columns=['term'])
                 df.to_csv(term_dir + el['acc'] + '.tsv', sep='\t')
+
+        if flag_term == False:
+            print(term + ' not found')
 
 def parse_file_proteins_features(term, feature):
     count = 0
@@ -80,11 +135,14 @@ def calculate_jaccard_index(term, feature):
     term_dir = cfg.data['data'] + 'proteins_terms_features/' + term + '/'
 
     for filename in os.listdir(term_dir):
+        print(filename)
         df_file = pd.read_csv(term_dir + filename, sep='\t', index_col=[0])
         intersection = df_file.loc[(df_file.term == 1) & (df_file[feature] == 1)]
         union = df_file.loc[(df_file.term == 1) | (df_file[feature] == 1)]
-        jaccard = round(len(intersection) / len(union), 2)
-        # print(jaccard)
+        jaccard = 0
+        if len(intersection) != 0:
+            jaccard = round(len(intersection) / len(union), 2)
+            # print(jaccard)
         if jaccard != 0:
             dict_jaccard[term + '-' + feature].append(jaccard)
 
@@ -94,12 +152,20 @@ def calculate_jaccard_index(term, feature):
     ax = plt.gca()  # get axis handle
     p = ax.patches
     heights = [patch.get_height() for patch in p]
+    norm = np.linalg.norm(heights)
+
+    if norm == 0:
+        normal_heights = [0]*10
+    else:
+        normal_heights = heights / norm
+
+
     plt.close()
     row = []
     for j in range(10):
         row.append(term)
         row.append(j + 1)
-        row.append(math.floor(heights[j]))
+        row.append(round(normal_heights[j], 2))
 
     row_splitted = [row[i:i + 3] for i in range(0, len(row), 3)]
     return row_splitted
@@ -113,22 +179,24 @@ def generate_heatmaps_files(term, feature):
         if not file_exists:
             writer.writeheader()
         for row in rows:
-            writer.writerow({'term': row[0], 'jaccard': row[1], 'proteins': row[2], 'feature': feature})
+            writer.writerow({'term': terms_names[row[0]], 'jaccard': row[1], 'proteins': row[2], 'feature': feature})
 
 def generate_heatmaps(feature):
     term_feature = pd.read_csv(cfg.data['data'] + 'heatmaps/dataset.csv')
-    print(term_feature)
 
 
     df_range = term_feature.loc[(term_feature.feature == feature)]
 
     # reshape term_feature dataset in proper format to create seaborn heatmap
     term_feature_df = df_range.pivot('term', 'jaccard', 'proteins')
+    print(term_feature_df)
 
-    sns.heatmap(term_feature_df, cmap="BuPu")
+    sns.heatmap(term_feature_df, cmap="BuPu", xticklabels=True, yticklabels=True)
+
   # create seaborn heatmap
     plt.title(feature)
-    plt.savefig(cfg.data['data'] + 'heatmaps/range_' + feature + '.png')
+    plt.tick_params(labelsize=5)
+    plt.savefig(cfg.data['data'] + 'heatmaps/range_' + feature + '.png', bbox_inches='tight')
     plt.close()
 
 
