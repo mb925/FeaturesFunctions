9cb65102eb8becd95e5ffcb56c15413932511546
martina <martina.bevilacqua1992@gmail.com>
2021-06-09 17:52:09 +0200

    refactored parse_file_protein_terms to unique file

--- src/parser.py
+++ src/parser.py
@@ -61,79 +61,87 @@ terms_names = {
 
 
 def create_protein_terms_features_table(terms, features):
-
-    # for term in terms:
-    #     parse_file_protein_terms(term)
+    for term in terms:
+        parse_file_protein_terms(term)
 
     for feature in features:
         for term in terms:
             print(term)
             # add features to files
             # parse_file_proteins_features(term, feature)
-            generate_heatmap_dataset(term, feature)
-        generate_heatmaps(feature)
+        #     generate_heatmap_dataset(term, feature)
+        # generate_heatmaps(feature)
 
 
 def parse_file_protein_terms(term):
-    print(term)
 
     with open(cfg.data['data'] + 'disprot_regions.json') as json_file:
         data = json.load(json_file)
         dict_terms = {}
+        df = pd.DataFrame()
+        the_term_is_in_the_whole_file = False
+
         for el in data['data']:
-            flag_term = False
+            the_term_is_in_the_current_row = False
+
+            sequence_length = len(el['sequence'])
 
             for region in el['regions']:
                 if region['term_id'] == term:
-                    flag_term = True
-                    for i in range(region['start'] - 1, region['end'] -1):
+                    the_term_is_in_the_whole_file = True
+                    the_term_is_in_the_current_row = True
+
+                    for i in range(region['start'] - 1, region['end'] - 1):
                         if el['acc'] in dict_terms:
                             dict_terms[el['acc']][i] = 1
                         else:
-                            sequence_length = len(el['sequence'])
                             dict_terms[el['acc']] = [0] * sequence_length
+            if the_term_is_in_the_current_row:
+                df_el = pd.DataFrame({
+                    'residue' : np.arange(1, sequence_length + 1, 1).tolist(),
+                    'acc': el['acc'],
+                    'term_code': terms_names[term],
+                    'term': dict_terms[el['acc']]
+                })
+                df = df.append(df_el)
 
-            if flag_term == True:
-                term_dir = cfg.data['data'] + 'proteins_terms_features/' + term + '/'
-                if not os.path.exists(term_dir):
-                    os.makedirs(term_dir)
-                df = pd.DataFrame(dict_terms[el['acc']], columns=['term'])
+        if the_term_is_in_the_whole_file == False:
+            print(term + ' not found')
 
-                df.to_csv(term_dir + el['acc'] + '.tsv', sep='\t')
+        file = cfg.data['data'] + 'terms_features.tsv'
+        df.to_csv(file, mode='a',  header=(not os.path.exists(file)), sep='\t', index=False)
 
-            if flag_term == False:
-                print(term + ' not found')
 
 
 
 def parse_file_proteins_features(term, feature):
-    count = 0
     df_features = pd.read_csv(cfg.data['data'] + 'mobidb_features.tsv', sep='\t')
     print(feature)
 
-    term_dir = cfg.data['data'] + 'proteins_terms_features/' + term + '/'
-    for filename in os.listdir(term_dir):
-        count += 1
-        acc = filename.split('.')[0]
+    # calculate features for each acc
+    file_dir = cfg.data['data'] + 'terms_features.tsv'
+    df_terms_features = pd.read_csv(file_dir, sep= '\t')
+    accessions = df_terms_features.acc.unique()
+    for acc in accessions:
         regions = df_features.loc[(df_features.acc == acc) & (df_features.feature == feature)]['start..end'].values
         if len(regions) > 0:
             regions = regions[0].split(',')
 
-        df_file = pd.read_csv(term_dir + filename, sep='\t', index_col=[0])
 
+        # write features to file
         # will be zero if feature is missing or if acc is missing in the mobidb file
-        feature_column = [0] * len(df_file)
+        feature_column = [0] * len(sequence_length)
         for region in regions:
             start = region.split('..')[0]
             end = region.split('..')[1]
             for i in range(int(start) - 1, int(end) - 1):
                 feature_column[i] = 1
-        df_file[feature] = feature_column
+        df_terms_features[feature] = feature_column
 
-        df_file.to_csv(term_dir + filename, sep='\t')
+        df_terms_features.to_csv(term_dir + filename, sep='\t')
+
 
 def calculate_jaccard_index(term, feature):
-
     dict_jaccard = {term + '-' + feature: []}
     term_dir = cfg.data['data'] + 'proteins_terms_features/' + term + '/'
 
@@ -147,10 +155,6 @@ def calculate_jaccard_index(term, feature):
             # print(jaccard)
         dict_jaccard[term + '-' + feature].append(round(jaccard, 1))
 
-    print('jaccard')
-    print(dict_jaccard[term + '-' + feature])
-    print('histobins')
-
 
     row = []
     # how many times each step happens
@@ -162,7 +166,7 @@ def calculate_jaccard_index(term, feature):
 
     print(total_occurrences)
     for x in range(10):
-        row.append(term)
+        row.append(term)  # TODO: add quantity of proteins for each term
         row.append(x + 1)
         # normalizing the occurrences to the range 0 1
         row.append(total_occurrences[x] / sum(total_occurrences))
@@ -171,34 +175,35 @@ def calculate_jaccard_index(term, feature):
 
     return row_splitted
 
+
 def generate_heatmap_dataset(term, feature):
     rows = calculate_jaccard_index(term, feature)
-    headers = ['term', 'jaccard', 'proteins', 'feature']
+    headers = ['term', 'overlap', 'proteins', 'feature']
     file_exists = os.path.isfile(cfg.data['data'] + 'heatmaps/dataset.csv')
     with open(cfg.data['data'] + 'heatmaps/dataset.csv', "a", newline="") as f:
         writer = csv.DictWriter(f, delimiter=',', lineterminator='\n', fieldnames=headers)
         if not file_exists:
             writer.writeheader()
         for row in rows:
-            writer.writerow({'term': terms_names[row[0]], 'jaccard': row[1], 'proteins': row[2], 'feature': feature})
+            writer.writerow({'term': terms_names[row[0]], 'overlap': row[1], 'proteins': row[2], 'feature': feature})
+
 
 def generate_heatmaps(feature):
     term_feature = pd.read_csv(cfg.data['data'] + 'heatmaps/dataset.csv')
 
-
     df_range = term_feature.loc[(term_feature.feature == feature)]
 
     # reshape term_feature dataset in proper format to create seaborn heatmap
-    term_feature_df = df_range.pivot('term', 'jaccard', 'proteins')
-
+    term_feature_df = df_range.pivot('term', 'overlap', 'proteins')
+    fig, ax = plt.subplots(1, 1, figsize=(10, 10), dpi=300)
+    sns.set(font_scale=1)
     sns.heatmap(term_feature_df, cmap="BuPu", xticklabels=True, yticklabels=True)
-
-  # create seaborn heatmap
+    ax.set_ylabel('')
+    # create seaborn heatmap
     plt.title(feature)
-    plt.tick_params(labelsize=5)
+    plt.tick_params(labelsize=10)
     plt.savefig(cfg.data['data'] + 'heatmaps/range_' + feature + '.png', bbox_inches='tight')
     plt.close()
-
 
 
 def draw_barplot(x, y):
